d09f549f06507f840af24065d85c2e50faeeb353
git <admin@lianyue.org>
2016-03-27 17:48:53 +0900

    `

---
+++ examples/config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// Oauth  的配置
+
+//  Api Key    Client Id
+define('CLIENT_ID', '');
+
+
+// Client Key  Secret Key
+define('CLIENT_SELECT', '');
+
+
+// 当前目录的url 路径 记得结尾加 ／
+define('URI_BASE', 'http://www.qqapi.dev/examples');
+
+
+
+require dirname(__DIR__) . '/autoload.php';
